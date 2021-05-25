fc7f37005d17c47544d3278f7a852739440e390e
Andreco <320266241@alumno.enp.unam.mx>
2021-05-24 19:05:06 -0500

    FEAT: menu de personalizacion y pausa de animacion

--- script.js
+++ script.js
@@ -2,8 +2,39 @@ var boton1 = document.getElementById("boton1");
 
 boton1.addEventListener("click", ()=>{
   var ojo = document.getElementById("derecha");
-  ojo.style.animationPlayState = "running";
-  var ojo = document.getElementById("izquierda");
-  ojo.style.animationPlayState = "running";
-  console.log(ojo.style);
+  if(ojo.style.animationPlayState == "running")
+  {
+    var ojo = document.getElementById("derecha");
+    ojo.style.animationPlayState = "paused";
+    var ojo = document.getElementById("izquierda");
+    ojo.style.animationPlayState = "paused";
+  }
+  else
+  {
+    ojo.style.animationPlayState = "running";
+    var ojo = document.getElementById("izquierda");
+    ojo.style.animationPlayState = "running";
+  }
+})
+
+var rojo = document.getElementById("rojo"); 
+var amarillo = document.getElementById("amarillo"); 
+var verde = document.getElementById("verde"); 
+var azul = document.getElementById("azul"); 
+var interior = document.getElementById("interior")
+
+rojo.addEventListener("click", ()=>{
+  interior.style.backgroundColor = "red";
+})
+
+amarillo.addEventListener("click", ()=>{
+  interior.style.backgroundColor = "yellow";
+})
+
+verde.addEventListener("click", ()=>{
+  interior.style.backgroundColor = "#00cc00";
+})
+
+azul.addEventListener("click", ()=>{
+  interior.style.backgroundColor = "blue";
 })
